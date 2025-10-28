4b0a31310f3b0b4f47918004fbf625b9d38c3541
Christian <paul.chris.dev@gmail.com>
2025-10-28 18:27:00 +0800

    add boiler

---
+++ src/main/java/com/example/tracker/ProductStockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockTrackerApplication.class, args);
+	}
+
+}
